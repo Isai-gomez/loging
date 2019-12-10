d1c14979bf4fca59ad473b56ca9fcc7666709007
Chai <isaigomezhernandez.com>
2019-12-10 00:21:55 -0600

    Component google and facebook

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,32 @@
-import React from "react";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>My app</h1>
-    </div>
-  );
+import React, { Component } from "react";
+import FacebookLogin from "react-facebook-login";
+import GoogleLogin from "react-google-login";
+class App extends Component {
+  render() {
+    const responseFacebook = response => {
+      console.log(response);
+    };
+    const responseGoogle = response => {
+      console.log(response);
+    };
+    return (
+      <div className="App">
+        <h1>LOFIN WITH FACEBOOK AND GOOGLE</h1>
+        <FacebookLogin
+          appId="" // app id no created yet
+          fields="name, email,picture"
+          callback={responseFacebook}
+        ></FacebookLogin>
+        <br />
+        <br />
+        <GoogleLogin
+          clientId="" // clientID not created yet
+          buttoText="LOGIN WHIT GOOGLE"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        ></GoogleLogin>
+      </div>
+    );
+  }
 }
-
 export default App;
